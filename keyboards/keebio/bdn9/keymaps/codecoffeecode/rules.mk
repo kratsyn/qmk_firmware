b53bf462d676f7a9c8fb06ff342153a4f36c7e58
kratsyn <synkrat@gmail.com>
2020-04-06 01:27:04 -0500

    modify codecoffeecode keymap to allow compiling for avr

--- keyboards/keebio/bdn9/keymaps/codecoffeecode/rules.mk
+++ keyboards/keebio/bdn9/keymaps/codecoffeecode/rules.mk
@@ -1,4 +1,10 @@
+<<<<<<< HEAD
 MOUSEKEY_ENABLE = yes
 ifneq (, $(findstring rev1, $(KEYBOARD)))
   CTPC = yes
 endif
+=======
+MOUSEKEY_ENABLE = yes 
+LTO_ENABLE=yes      # compact this firmware a bit so it will pass avr size check
+CTCP=yes
+>>>>>>> a2b998919... modify codecoffeecode keymap to allow compiling for avr
