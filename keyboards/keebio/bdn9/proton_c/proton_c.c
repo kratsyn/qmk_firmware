4f18c00be73aad4837e2f7164cefb63278035783
kratsyn <synkrat@gmail.com>
2020-04-06 02:09:11 -0500

    forgot one.

--- keyboards/keebio/bdn9/proton_c/proton_c.c
+++ keyboards/keebio/bdn9/proton_c/proton_c.c
@@ -13,8 +13,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include <hal.h>
 #include "proton_c.h"
-#include "hal.h"
 
 #define LED_ON()        palSetLine(C13);
 #define LED_OFF()       palClearLine(C13);
