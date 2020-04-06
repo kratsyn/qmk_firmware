384be06df91ff8c63193cd67c8aafc105372234f
kratsyn <synkrat@gmail.com>
2020-04-06 02:03:20 -0500

    more better include definition

--- keyboards/keebio/bdn9/proton_c/backlight_driver.c
+++ keyboards/keebio/bdn9/proton_c/backlight_driver.c
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "hal.h"
+#include <hal.h>
 #include "debug.h"
 #include "eeconfig.h"
 #include "backlight.h"
