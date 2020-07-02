2a0973df0bdfcf482ceef2d20bcd7bd6ff800d43
kratsyn <synkrat@gmail.com>
2020-07-02 15:43:58 -0500

    prep for rebase

--- keyboards/keebio/bdn9/bdn9.h
+++ keyboards/keebio/bdn9/bdn9.h
@@ -15,8 +15,8 @@
  */
 #pragma once
 
-#ifdef KEYBOARD_keebio_bdn9_rev1
-    #include "rev1.h"
+#ifdef KEYBOARD_keebio_bdn9_avr
+    #include "avr.h"
 #endif
 #ifdef KEYBOARD_keebio_bdn9_proton_c
     #include "proton_c.h"
