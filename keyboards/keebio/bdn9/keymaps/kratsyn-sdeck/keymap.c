a6d46530f0754150f54a8e06e397c89f448c11b1
kratsyn <synkrat@gmail.com>
2020-07-02 15:01:22 -0500

    Updates for via mapping control and some layer lighting (pending rebase).

--- keyboards/keebio/bdn9/keymaps/kratsyn-sdeck/keymap.c
+++ keyboards/keebio/bdn9/keymaps/kratsyn-sdeck/keymap.c
@@ -23,6 +23,13 @@ typedef struct {
 enum { SINGLE_TAP = 1, DOUBLE_TAP = 2, TRIPLE_TAP = 3, QUAD_TAP = 4, QUIN_TAP = 5 };
 enum { TD_LAYR = 0 };
 
+const rgblight_segment_t PROGMEM        default_layer[]   = RGBLIGHT_LAYER_SEGMENTS({0, 4, HSV_PURPLE});
+const rgblight_segment_t PROGMEM        macro_layer[]     = RGBLIGHT_LAYER_SEGMENTS({0, 1, HSV_PURPLE});
+const rgblight_segment_t PROGMEM        backlight_layer[] = RGBLIGHT_LAYER_SEGMENTS({1, 1, HSV_PURPLE});
+const rgblight_segment_t PROGMEM        rgb_layer[]       = RGBLIGHT_LAYER_SEGMENTS({2, 1, HSV_PURPLE});
+const rgblight_segment_t PROGMEM        quantum_layer[]   = RGBLIGHT_LAYER_SEGMENTS({3, 1, HSV_RED});
+const rgblight_segment_t *const PROGMEM lighting_layers[] = RGBLIGHT_LAYERS_LIST(default_layer, macro_layer, backlight_layer, rgb_layer, quantum_layer);
+
 int  cur_dance(qk_tap_dance_state_t *state);
 void ql_finished(qk_tap_dance_state_t *state, void *user_data);
 void ql_reset(qk_tap_dance_state_t *state, void *user_date);
@@ -31,14 +38,14 @@ void ql_reset(qk_tap_dance_state_t *state, void *user_date);
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // User 0 (VIA MAPPED)
     [0] = LAYOUT(
-        _______, TD(TD_LAYR), KC_MUTE,
-        _______, KC_UP, _______,
+        KC_MPLY, TD(TD_LAYR), KC_MUTE,
+        KC_PGUP, KC_UP, KC_PGDOWN,
         KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [1] = LAYOUT(
         _______, _______, _______,
-        _______, KC_PGUP, _______,
-        _______, KC_PGDOWN, _______
+        _______, _______, _______,
+        _______, _______, _______
     ),
     // LED Backlight
     [2] = LAYOUT(
@@ -49,14 +56,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // RGB Underglow
     [3] = LAYOUT(
         RGB_TOG, _______, RGB_MOD, 
-        KC_NO, RGB_VAI, KC_NO,
+        RGB_SPI, RGB_VAI, RGB_SPD,
         RGB_MOD, RGB_VAD, RGB_RMOD
     ),
     // Quantum
     [4] = LAYOUT(
-        KC_NO, _______, KC_NO,
+        KC_NO, _______, EEP_RST,
         KC_NO, RESET, KC_NO,
-        DEBUG, EEP_RST, KC_NO
+        KC_NO, DEBUG, KC_NO
     )
 };
 // clang-format on
@@ -71,10 +78,10 @@ void encoder_update_user(uint8_t index, bool clockwise) {
             default:
                 tap_code((clockwise) ? KC_WH_D : KC_WH_U);
                 break;
-			// F13 - F14 Macro
-			case 1:
-				tap_code((clockwise) ? KC_F13 : KC_F14);
-				break;
+            // F13 - F14
+            case 1:
+                tap_code((clockwise) ? KC_F13 : KC_F14);
+                break;
             // backlight on layer 2
             case 2:
                 (clockwise) ? backlight_increase() : backlight_decrease();
@@ -84,17 +91,17 @@ void encoder_update_user(uint8_t index, bool clockwise) {
                 (clockwise) ? rgblight_increase_hue() : rgblight_decrease_hue();
                 break;
         }
-    // Encoder 1
+        // Encoder 1
     } else if (index == 1) {
         switch (layer) {
             // volume as default
             default:
                 tap_code((clockwise) ? KC_VOLU : KC_VOLD);
                 break;
-			// F15 - F16 Macro
-			case 1:
-				tap_code((clockwise) ? KC_F15 : KC_F16);
-				break;
+            // F15 - F16
+            case 1:
+                tap_code((clockwise) ? KC_F15 : KC_F16);
+                break;
             // rgb saturation on layer 3
             case 3:
                 (clockwise) ? rgblight_increase_sat() : rgblight_decrease_sat();
@@ -106,6 +113,8 @@ void encoder_update_user(uint8_t index, bool clockwise) {
 void eeconfig_init_user(void) {  // EEPROM Reset
     dprint("[SYS] EEPROM Reset to default values.\n");
 }
+
+void keyboard_post_init_user(void) { rgblight_layers = lighting_layers; }
 
 int cur_dance(qk_tap_dance_state_t *state) {
     if (state->count == 1) {
@@ -132,27 +141,27 @@ void ql_finished(qk_tap_dance_state_t *state, void *user_data) {
     ql_tap_state.state = cur_dance(state);
     switch (ql_tap_state.state) {
         case SINGLE_TAP:
-            dprint("[TD] Layer change. (0)\n");
             layer_move(0);
+            rgblight_blink_layer(default_layer, 1000);
             break;
         case DOUBLE_TAP:
-            dprint("[TD] Layer change. (1)\n");
             layer_on(1);
+            rgblight_blink_layer(macro_layer, 1000);
             break;
         case TRIPLE_TAP:
-            dprint("[TD] Layer change. (2)\n");
             layer_on(2);
+            rgblight_blink_layer(backlight_layer, 1000);
             break;
         case QUAD_TAP:
-            dprint("[TD] Layer change. (3)\n");
             layer_on(3);
+            rgblight_blink_layer(rgb_layer, 1000);
             break;
         case QUIN_TAP:
-            dprint("[TD] Layer change. (4)\n");
             layer_on(4);
+            rgblight_blink_layer(quantum_layer, 1000);
             break;
     }
 }
 
-void ql_reset(qk_tap_dance_state_t *state, void *user_data) { ql_tap_state.state = 0; }
+void                  ql_reset(qk_tap_dance_state_t *state, void *user_data) { ql_tap_state.state = 0; }
 qk_tap_dance_action_t tap_dance_actions[] = {[TD_LAYR] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, ql_finished, ql_reset)};
